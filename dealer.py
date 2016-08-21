eee1a4ead20bf5a6b89b315394ac9ccc3854df5d
Jon Simonsen <jonsimonsen@users.noreply.github.com>
2016-08-21 16:28:59 +0200

    Added functionality to deal hands, and refatored card printing (version 2.8)

--- dealer.py
+++ dealer.py
@@ -1,6 +1,10 @@
+#Other libs
+from random import randint
+
 #Global constants
 SUITS = 4
 VALUES = 13
+HAND_SIZE = 5
 
 #Classes
 
@@ -13,6 +17,40 @@ class Card(object):
         self._suit = suit
         self._value = value
 
+    def printCard(self):
+        """Print info about the card"""
+
+        rank = ""
+        color = ""
+
+        if self._value == 1:
+            rank = "A"
+        elif self._value < 10:
+            rank = str(self._value)
+        elif self._value == 10:
+            rank = "T"
+        elif self._value == 11:
+            rank = "J"
+        elif self._value == 12:
+            rank = "Q"
+        elif self._value == 13:
+            rank = "K"
+        else:
+            rank = "Illegal rank"
+
+        if self._suit == 0:
+            color = "clubs"
+        elif self._suit == 1:
+            color = "diamonds"
+        elif self._suit == 2:
+            color = "hearts"
+        elif self._suit == 3:
+            color = "spades"
+        else:
+            color = "illegal suit"
+
+        print("\t" + rank + " of " + color)
+
 class Dealer(object):
     """A poker dealer"""
 
@@ -21,6 +59,9 @@ class Dealer(object):
 
         self._cards = list()
         self.getCards()
+
+        self._deck = list()
+        self.resetDeck()
 
     def getCards(self):
         """Initialize the cards with the 52 cards of a deck"""
@@ -31,37 +72,31 @@ class Dealer(object):
                 self._cards.append(newCard)
 
     def printCards(self):
-        """Print the cards of the sorted deck"""
-
-        rank = ""
-        color = ""
-        res = ""
+        """Print the cards of the entire deck"""
 
         for card in self._cards:
-            if card._value == 1:
-                rank = "A"
-            elif card._value < 10:
-                rank = str(card._value)
-            elif card._value == 10:
-                rank = "T"
-            elif card._value == 11:
-                rank = "J"
-            elif card._value == 12:
-                rank = "Q"
-            elif card._value == 13:
-                rank = "K"
-            else:
-                rank = "Illegal rank"
+            card.printCard()
 
-            if card._suit == 0:
-                color = "clubs"
-            elif card._suit == 1:
-                color = "diamonds"
-            elif card._suit == 2:
-                color = "hearts"
-            elif card._suit == 3:
-                color = "spades"
-            else:
-                color = "illegal suit"
+    def printDeck(self):
+        """Print the cards left in the deck"""
 
-            print(rank + " of " + color)
+        for card in self._deck:
+            card.printCard()
+
+    def resetDeck(self):
+        """Make _deck a copy of _cards"""
+
+        print("Shuffling cards...")
+        self._deck = list(self._cards)
+
+    def dealHand(self):
+        """Deal and display a hand from the deck"""
+
+        print("\nYour hand:")
+
+        for i in range(HAND_SIZE):
+            card = self._deck.pop(randint(0, len(self._deck) - 1))
+            card.printCard()
+
+        #self.printCards()
+        #self.printDeck()
--- one_hand.py
+++ one_hand.py
@@ -3,4 +3,11 @@ from dealer import *
 
 #initialise variables
 cardRack = Dealer()
+
+#main loop
 cardRack.printCards()
+
+for i in range(5):
+    for j in range(5):
+        cardRack.dealHand()
+    cardRack.resetDeck()
